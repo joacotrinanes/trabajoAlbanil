3ef2e5666b20c3aee4a6e0ec0eb33b999d525574
joacotrinanes <joaco.trinanes@gmail.com>
2023-04-29 17:44:58 -0300

    Added Whatsapp floating logo

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/navBar/NavBar.js';
 import MainContent from './components/mainContent/MainContent';
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <MainContent/>
+      <FloatingWhatsApp phoneNumber='+59899591708' accountName='Joaco Triñanes' />
     </div>
   );
 }
